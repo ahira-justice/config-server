67890f8a5c788ca228b192818b3b20d9786ea3d4
Ahira Adefokun <justiceahira@gmail.com>
2023-10-24 19:03:00 +0100

    Remove base url from auth exclusion

--- src/main/java/com/ahirajustice/configserver/common/constants/SecurityConstants.java
+++ src/main/java/com/ahirajustice/configserver/common/constants/SecurityConstants.java
@@ -10,12 +10,10 @@ public class SecurityConstants {
     public static final String HEADER_STRING = "Authorization";
     public static final String[] EXCLUDE_FROM_AUTH_URLS = new String[] {
             "/**, OPTIONS",
-            "/, GET",
             "/api/auth/token, POST",
     };
     public static final String[] EXCLUDE_FROM_REQUEST_RESPONSE_LOGGER = new String[] {
             "/**, OPTIONS",
-            "/, GET",
             "/api/auth/token, POST",
             "/api/users, POST",
     };
